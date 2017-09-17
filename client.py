9b5cf0c843d26d94f31b92b8904d4a7d42e3be0a
Mandingon <daniellarsson054@gmail.com>
2017-09-17 16:57:34 +0200

    Fixed

--- client.py
+++ client.py
@@ -4,30 +4,35 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-#confirm = input("Do you want to connect?[y/n]")
+confirm = raw_input("Do you want to connect?[y/n]\n")
 
-#if(confirm == 'y'):
+if(confirm == 'y'):
 
-#Connect to the port that the server is listening 
-server_adress = ('localhost', 11111)
-print>>sys.stderr, 'connecting to %s port %s' % server_adress
-sock.connect(server_adress) 
+	#Connect to the port that the server is listening 
+	server_adress = ('localhost', 11111)
+	print>>sys.stderr, 'connecting to %s port %s' % server_adress
+	sock.connect(server_adress) 
+	
+	while True:
+		try:
+			#send data
+			message = raw_input("What do you want to send?\n")
+			if not message:
+				print>>sys.stderr, 'closing socket'
+				sock.close()
+				break
+			print>>sys.stderr, 'sending "%s"' % message
+			sock.sendall(message)
 
-try: 
-	#send data
-	message = 'This is the message. It will be repeated.'
-	print>>sys.stderr, 'sending "%s"' % message
-	sock.sendall(message)
+			#look for response from server
+			amount_received = 0
+			amount_expected = len(message)
 
-	#look for response from server
-	amount_received = 0
-	amount_expected = len(message)
-
-	while amount_received < amount_expected:
-		data  = sock.recv(16)
-		amount_received += len(data)
-		print>>sys.stderr, 'received "%s"' % data
-
-finally:
-	print>>sys.stderr, 'closing socket'
-	sock.close()
+			while amount_received < amount_expected:
+				data  = sock.recv(128)
+				amount_received += len(data)
+				print>>sys.stderr, 'received "%s"' % data
+		finally:
+			print>>sys.stderr, "Continuing connection..."	
+				
+print >> sys.stderr, 'Exiting...'
