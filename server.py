d9cca4215ea3be5d3642c93597703dec2c679330
Discostewe <mw-p95@hotmail.com>
2017-09-17 14:36:41 +0200

    first step on server

--- server.py
+++ server.py
@@ -1,2 +1,12 @@
 import socket
 import sys
+
+#Create a tcp/ip socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Bind the socket to port 11111
+
+server_adress = ('localhost', 11111)
+print >>sys.stderr, 'starting up on %s port %s' % server_adress
+sock.bind(server_adress)
