4e6a823af6f63f38e3b493a36135ae5fe7d3c265
martwahl05 <martin_p95@hotmail.se>
2017-09-26 09:41:42 +0200

    encryption-non-deterministic

--- encrypt.py
+++ encrypt.py
@@ -16,36 +16,43 @@ def encrypt(password,message):
     pad = lambda s: s + ((BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING)
     
     #Produces a ciphertext that consists of (A-Z, a-z, 0-9,+,/
-    EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+    EncodeAES = lambda c, s, iv: base64.b64encode(iv + c.encrypt(pad(s)))
     
     #hashes the password with sha256 to get a encryptionkey of 32 bytes
     key = hashlib.sha256(str(password).encode()).digest()
     
+    iv = Random.new().read(AES.block_size)
     #Creates the cipher that the messege is to be encrypted with
-    cipher = AES.new(key)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
     
     #Encrypts the message with the cipher and produces a ciphertext
 
-    ciphertext = EncodeAES(cipher, message)
-    #print('Encrypted string:', ciphertext, file=sys.stderr)
-    print>>sys.stderr, 'length of string: %s' % len(message)
+    ciphertext = EncodeAES(cipher, message, iv)
+    print>>sys.stderr, 'Encrypted: %s' % ciphertext
     return ciphertext
 
 def decrypt (password,ciphertext):
     PADDING = '{'
 
-    DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+    DecodeAES = lambda c, e: c.decrypt(e).rstrip(PADDING)
   
     key = hashlib.sha256(str(password).encode()).digest()
-   
-    cipher = AES.new(key)
     
-    decoded = DecodeAES(cipher, ciphertext)
+    ciphertext = base64.b64decode(ciphertext)
+
+    iv = ciphertext[:AES.block_size]
+
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    
+    decoded = DecodeAES(cipher, ciphertext[16:])
 
    #print('Decrypted string:', decoded, file=sys.stderr)
+    print>>sys.stderr, 'Decrypted: %s' % decoded
+
     return decoded
+
 def diffyhellman(gen, prime, i):
 	#gen and prim are agreed upon values and i is a private secret
 	return gen**i % prime
 
-
+decrypt(3, encrypt(3, 'hej'))
